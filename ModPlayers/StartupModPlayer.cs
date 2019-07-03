fd55ed3bc51ce6daffd2a4ff531e5c099978f74f
Caio Oliveira <colive.dev@gmail.com>
2019-07-03 02:03:15 -0300

    added zeny into StartInventorySetup

---
+++ ModPlayers/StartupModPlayer.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+
+
+namespace TRPG.ModPlayers
+{
+    class StartupModPlayer : ModPlayer
+    {
+        public override void SetupStartInventory(IList<Item> items, bool mediumcoreDeath)
+        {
+            Item item = new Item();
+            item.SetDefaults(mod.ItemType("Zeny")); // ItemID.Wood (if you wants to add a vanilla item from terraria)
+            item.stack = 1;
+            items.Add(item);
+        }
+    }
+}
+
+
