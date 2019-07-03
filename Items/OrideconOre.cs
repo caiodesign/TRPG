aae3c8c5d1784631b788fbff15845ae3d30afd09
Caio Oliveira <colive.dev@gmail.com>
2019-07-03 18:59:07 -0300

    added new ore called OrideconOre, a stone sended by the gods

---
+++ Items/OrideconOre.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TRPG.Items
+{
+    class OrideconOre : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Oridecon ore");
+            Tooltip.SetDefault("Pure Oridecon metal. Widely known as the Blessed Metal, it can craft god items.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 26;
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.useStyle = 1;
+            item.knockBack = .1f;
+            item.value = 50;
+            item.rare = -15;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+            item.consumable = true;
+            item.createTile = mod.TileType("OrideconOreTile");
+            item.maxStack = 999;
+        }
+    }
+}
---
+++ Tiles/OrideconOreTile.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace TRPG.Tiles
+{
+    class OrideconOreTile : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileLighted[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            drop = mod.ItemType("OrideconOre");
+            minPick = 90;
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Oridecon Ore");
+            AddMapEntry(new Color(59, 160, 255), name);            
+        }
+
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            r = 0.26f;
+            g = 0.6f;
+            b = 1;
+        }
+    }
+}
