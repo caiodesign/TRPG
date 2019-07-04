a09e9d895eee35661fa0deb0ef9556ef6206feeb
Caio Oliveira <colive.dev@gmail.com>
2019-07-03 21:41:15 -0300

    added Oridecon Bar

--- Items/Oridecon/OrideconOre.cs
+++ Items/Oridecon/OrideconOre.cs
@@ -2,26 +2,26 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace TRPG.Items
+namespace TRPG.Items.Oridecon
 {
     class OrideconOre : ModItem
     {
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Oridecon ore");
-            Tooltip.SetDefault("Pure Oridecon metal. Widely known as the Blessed Metal, it can craft god items.");
+            Tooltip.SetDefault("Pure Oridecon metal. Widely known as the Blessed Metal.");
         }
 
         public override void SetDefaults()
         {
-            item.width = 22;
-            item.height = 26;
+            item.width = 26;
+            item.height = 22;
             item.useTime = 20;
             item.useAnimation = 20;
             item.useStyle = 1;
             item.knockBack = .1f;
             item.value = 50;
-            item.rare = -15;
+            item.rare = 9;
             item.UseSound = SoundID.Item1;
             item.autoReuse = true;
             item.consumable = true;
---
+++ Items/Oridecon/Orideconbar.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TRPG.Items.Oridecon
+{
+    class OrideconBar : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Oridecon bar");
+            Tooltip.SetDefault("Bar of oridecon ore. Widely known as the Blessed Metal, it can craft god items.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 30;
+            item.height = 24;
+            item.useStyle = 1;
+            item.value = 500;
+            item.rare = 10;
+            item.autoReuse = true;
+            item.consumable = true;
+            item.maxStack = 99;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe newRecipe = new ModRecipe(mod);
+
+            newRecipe.AddIngredient(null, "OrideconOre", 3);
+            newRecipe.AddTile(TileID.Furnaces);
+            newRecipe.SetResult(this);
+            newRecipe.AddRecipe();
+        }
+    }
+}
